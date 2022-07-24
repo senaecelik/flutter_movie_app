f776c24afe6bef749e04516c3564bbc5ec42b6d8
Sena Çelik <senaecelik@gmail.com>
2022-07-24 15:49:31 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
+import 'dart:io';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/res/color.dart';
 import 'package:flutter_application/utils/routes/routes.dart';
 import 'package:flutter_application/utils/routes/routes_name.dart';
-import 'package:flutter_application/view/login_view.dart';
 import 'package:flutter_application/view_model/auth_view_model.dart';
+import 'package:flutter_application/view_model/detail_view_model.dart';
 import 'package:flutter_application/view_model/home_view_model.dart';
+import 'package:flutter_application/view_model/person_detail_view_model.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialization(null);
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const MyApp());
+}
+
+Future initialization(
+    BuildContext? context) async {
+  await Future.delayed(Duration(seconds: 4));
 }
 
 class MyApp extends StatelessWidget {
@@ -23,31 +34,46 @@ class MyApp extends StatelessWidget {
           create: (_) => AuthViewModel(),
         ),
         ChangeNotifierProvider(
-            create: (_) => HomeViewViewModel())
+            create: (_) => HomeViewViewModel()),
+        ChangeNotifierProvider(
+            create: (_) => DetailViewModel()),
+        ChangeNotifierProvider<PersonDetailViewModel>.value(
+          value: PersonDetailViewModel())
       ],
       child: MaterialApp(
         scrollBehavior: CustomScrollBehavior(),
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.dark,
         theme: ThemeData(
-          appBarTheme: AppBarTheme(
-              backgroundColor: Colors.transparent,
-              elevation: 0),
           scaffoldBackgroundColor:
               AppColors.blackColor,
         ),
-        initialRoute: RoutesName.home,
+        initialRoute: RoutesName.onboard,
         onGenerateRoute: Routes.generateRoute,
+        
       ),
     );
   }
 }
 
-class CustomScrollBehavior extends MaterialScrollBehavior {
+class CustomScrollBehavior
+    extends MaterialScrollBehavior {
   // Override behavior methods and getters like dragDevices
   @override
   Set<PointerDeviceKind> get dragDevices => {
         PointerDeviceKind.touch,
         PointerDeviceKind.mouse,
       };
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(
+      SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host,
+                  int port) =>
+              true;
+  }
 }
