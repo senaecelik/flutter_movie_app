f7fcc1f8807be06a39769749bb4e7c5a108a328c
Sena Çelik <senaecelik@gmail.com>
2022-07-24 15:49:49 +0300

    Create avaible_circle_photo.dart

---
+++ lib/res/componenets/avaible_circle_photo.dart
@@ -0,0 +1,32 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application/models/cast.dart';
+import 'package:flutter_application/res/app_url.dart';
+
+class AvaibleCirclePhoto extends StatelessWidget {
+  final String imageUrl;
+  const AvaibleCirclePhoto({
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      fit: BoxFit.cover,
+      imageBuilder: (context, imageProvider) =>
+          Container(
+        margin: EdgeInsets.only(right: 10),
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover),
+        ),
+      ),
+      imageUrl: "${AppUrl.photoBaseUrl}$imageUrl",
+    );
+  }
+}
