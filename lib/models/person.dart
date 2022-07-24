a310af620b79a1ab44b0112697c589e0098c6548
Sena Çelik <senaecelik@gmail.com>
2022-07-24 15:49:16 +0300

    Create person.dart

---
+++ lib/models/person.dart
@@ -0,0 +1,68 @@
+class Person {
+  bool? adult;
+  List<String>? alsoKnownAs;
+  String? biography;
+  String? birthday;
+  String? deathday;
+  int? gender;
+  String? homepage;
+  int? id;
+  String? imdbId;
+  String? knownForDepartment;
+  String? name;
+  String? placeOfBirth;
+  double? popularity;
+  String? profilePath;
+
+  Person(
+      {this.adult,
+      this.alsoKnownAs,
+      this.biography,
+      this.birthday,
+      this.deathday,
+      this.gender,
+      this.homepage,
+      this.id,
+      this.imdbId,
+      this.knownForDepartment,
+      this.name,
+      this.placeOfBirth,
+      this.popularity,
+      this.profilePath});
+
+  Person.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    alsoKnownAs = json['also_known_as'].cast<String>();
+    biography = json['biography'];
+    birthday = json['birthday'];
+    deathday = json['deathday'];
+    gender = json['gender'];
+    homepage = json['homepage'];
+    id = json['id'];
+    imdbId = json['imdb_id'];
+    knownForDepartment = json['known_for_department'];
+    name = json['name'];
+    placeOfBirth = json['place_of_birth'];
+    popularity = json['popularity'];
+    profilePath = json['profile_path'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['adult'] = adult;
+    data['also_known_as'] = alsoKnownAs;
+    data['biography'] = biography;
+    data['birthday'] = birthday;
+    data['deathday'] = deathday;
+    data['gender'] = gender;
+    data['homepage'] = homepage;
+    data['id'] = id;
+    data['imdb_id'] = imdbId;
+    data['known_for_department'] = knownForDepartment;
+    data['name'] = name;
+    data['place_of_birth'] = placeOfBirth;
+    data['popularity'] = popularity;
+    data['profile_path'] = profilePath;
+    return data;
+  }
+}
