784709924e9f59fbfe9eedbc3b1615f2933ffe1c
Sena Çelik <senaecelik@gmail.com>
2022-08-02 13:31:32 +0300

    Update cast.dart

--- lib/models/cast.dart
+++ lib/models/cast.dart
@@ -1,23 +1,30 @@
 import 'package:flutter_application/models/person.dart';
 import 'package:json_annotation/json_annotation.dart';
-part 'cast.g.dart';
-
-@JsonSerializable()
 class Cast {
   List<Casts>? results;
-  Cast({this.results});
 
-  factory Cast.fromJson(
-      Map<String, dynamic> json) {
-    return _$CastFromJson(json);
+  Cast.fromJson(Map<String, dynamic> json) {
+    if (json['cast'] != null) {
+      results = <Casts>[];
+      json['cast'].forEach((v) {
+        results!.add(Casts.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
-    return _$CastToJson(this);
+    final Map<String, dynamic> data =
+        <String, dynamic>{};
+
+    if (results != null) {
+      data['cast'] = results!
+          .map((v) => v.toJson())
+          .toList();
+    }
+    return data;
   }
 }
 
-@JsonSerializable()
 class Casts extends Person {
   String? originalName;
   int? castId;
@@ -32,14 +39,97 @@ class Casts extends Person {
       this.creditId,
       this.order});
 
-  factory Casts.fromJson(
-      Map<String, dynamic> json) {
-    return _$CastsFromJson(json);
+  Casts.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    gender = json['gender'];
+    id = json['id'];
+    knownForDepartment =
+        json['known_for_department'];
+    name = json['name'];
+    popularity = json['popularity'];
+    profilePath = json['profile_path'];
+    castId = json['cast_id'];
+    character = json['character'];
+    creditId = json['credit_id'];
+    order = json['order'];
   }
 
   @override
   Map<String, dynamic> toJson() {
-    return _$CastsToJson(this);
+    final Map<String, dynamic> data =
+        new Map<String, dynamic>();
+    data['adult'] = adult;
+    data['gender'] = gender;
+    data['id'] = id;
+    data['known_for_department'] =
+        knownForDepartment;
+    data['name'] = name;
+    data['popularity'] = popularity;
+    data['profile_path'] = profilePath;
+    data['cast_id'] = castId;
+    data['character'] = character;
+    data['credit_id'] = creditId;
+    data['order'] = order;
+    return data;
   }
 }
 
+class Crew {
+  bool? adult;
+  int? gender;
+  int? id;
+  String? knownForDepartment;
+  String? name;
+  String? originalName;
+  double? popularity;
+  String? profilePath;
+  String? creditId;
+  String? department;
+  String? job;
+
+  Crew(
+      {this.adult,
+      this.gender,
+      this.id,
+      this.knownForDepartment,
+      this.name,
+      this.originalName,
+      this.popularity,
+      this.profilePath,
+      this.creditId,
+      this.department,
+      this.job});
+
+  Crew.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    gender = json['gender'];
+    id = json['id'];
+    knownForDepartment =
+        json['known_for_department'];
+    name = json['name'];
+    originalName = json['original_name'];
+    popularity = json['popularity'];
+    profilePath = json['profile_path'];
+    creditId = json['credit_id'];
+    department = json['department'];
+    job = json['job'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data =
+        new Map<String, dynamic>();
+    data['adult'] = adult;
+    data['gender'] = gender;
+    data['id'] = id;
+    data['known_for_department'] =
+        knownForDepartment;
+    data['name'] = name;
+    data['original_name'] = originalName;
+    data['popularity'] = popularity;
+    data['profile_path'] = profilePath;
+    data['credit_id'] = creditId;
+    data['department'] = department;
+    data['job'] = job;
+    return data;
+  }
+}
