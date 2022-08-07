aa8481b3e1b327e7d215caff4c1a84b8219eee16
Sena Çelik <senaecelik@gmail.com>
2022-08-07 16:36:54 +0300

    Update home_screen.dart

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/res/constant/string_constant.dart';
 import 'package:flutter_application/view/home/popular_movie_list/popular_list_view.dart';
 import 'package:flutter_application/view/home/top_rated_list/top_rated_list_view.dart';
 import 'package:flutter_application/view/home/up_coming_list/up_coming_list_view.dart';
@@ -15,11 +16,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title:
+            Text(AppString.instance.textAppName),
+        centerTitle: true,
+      ),
       body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
+          children: const [
             PopularListView(),
             TopRatedListView(),
             UpComingListView()
