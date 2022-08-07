381a549e44be34272eae8c00b271b960492a380c
Sena Çelik <senaecelik@gmail.com>
2022-08-07 16:36:43 +0300

    Update onboard_screen.dart

--- lib/view/onboard/onboard_screen.dart
+++ lib/view/onboard/onboard_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/res/color.dart';
-import 'package:flutter_application/res/componenets/red_button.dart';
-import 'package:flutter_application/res/style/text_style.dart';
+import 'package:flutter_application/res/componenets/title_text_widget.dart';
+import 'package:flutter_application/res/constant/string_constant.dart';
 import 'package:flutter_application/utils/routes/routes_name.dart';
+
+import '../../res/componenets/text_sub_widget.dart';
 
 class OnboardScreen extends StatelessWidget {
   const OnboardScreen({Key? key})
@@ -12,64 +13,65 @@ class OnboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final height =
         MediaQuery.of(context).size.height;
+
+    final width =
+        MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.fitWidth,
-                image: AssetImage(
-                    "assets/onboard.png"))),
-        child: Container(
-          margin: EdgeInsets.only(bottom: 30),
-          child: Column(
-            mainAxisAlignment:
-                MainAxisAlignment.end,
-            children: [
-              Text(
-                "Welcome to Mova",
-                style: AppStyle.instance.h2Bold,
-              ),
-              SizedBox(
-                height: height * .02,
-              ),
-              Text(
-                "The best movie streaming app of the century\n to make your days great!",
-                style: AppStyle
-                    .instance.bodyXLarge
-                    .copyWith(
-                        color: AppColors
-                            .whiteColor
-                            .withOpacity(0.5)),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: height * .02,
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: AppColors.redColor,
-                      fixedSize: Size(330, 50),
-                      shape:
-                          RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius
-                                      .circular(
-                                          30))),
-                  onPressed: () {
-                    Navigator.pushNamed(
-                        context, RoutesName.home);
-                  },
-                  child: Text(
-                    "Get Started",
-                    style: AppStyle
-                        .instance.bodyMedium,
-                  )),
-            ],
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          SizedBox(
+            height: height,
+            width: width,
+            child: Image.asset(
+              AppString.instance.pathOnboardImage,
+              fit: BoxFit.fitWidth,
+            ),
           ),
-        ),
+          _onboardBody(
+            height,
+            width,
+            context,
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _onboardBody(double height, double width,
+      BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        TitleWidget(
+            titleText:
+                AppString.instance.textWelcome),
+        SizedBox(
+          height: height * .02,
+        ),
+        TextSubWidget(
+            text: AppString
+                .instance.textOnboardSub),
+        SizedBox(
+          height: height * .02,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pushNamed(RoutesName.home);
+            },
+            child: SizedBox(
+              width: width * .4,
+              height: height * .065,
+              child: Center(
+                child: Text(AppString
+                    .instance.textGetStarted),
+              ),
+            )),
+        SizedBox(
+          height: height * .02,
+        ),
+      ],
     );
   }
 }
